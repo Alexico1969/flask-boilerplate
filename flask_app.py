5b12ae904dfaf4cf56c1db0343e9c5826115e5ec
Alex van Winkel <alexicoo@gmail.com>
2021-11-02 15:34:44 -0400

    transformed the static HTML site to a Flask site

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,9 @@
 from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__, static_url_path='/static')
-
-
 @app.route('/',methods=['GET','POST'])
 def home():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('index.html')
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
-
